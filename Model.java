1e320792c7c69754279f4fab8a5ceb2e5154fccc
bag-man <garland.owen@gmail.com>
2014-02-07 21:29:24 +0000

    4 steps forward, 10 back. More awesome code now though :)

--- Model.java
+++ Model.java
@@ -13,81 +13,31 @@ public class Model
 
   public void runTests()
   {
-    String students = loadFromTextFiles("students.txt"); 
-    parseStudents(students);
-  }
-
-  private void parseStudents(String inFile)
-  {
-    String uid, sName, fName, degree;
-    String[] tokens = inFile.split("[\n]");
-    int numRecords = Integer.parseInt(tokens[0]);
-    int recordCount = 1;
-    int i = 0;
-
-    while(i < numRecords)
-    {
-
-      //There must be a nicer way to do this.
-      uid = tokens[recordCount];
-      recordCount++;
-      fName = tokens[recordCount]; 
-      recordCount++;
-      sName = tokens[recordCount]; 
-      recordCount++;
-      degree = tokens[recordCount]; 
-      recordCount++;
-
-      System.out.println("Adding: " + uid + sName + fName + degree);
-      Students.add(new Student(uid, sName, fName, degree));
-
-      i++;
-    }
-  }
-
-  private String loadFromTextFiles(String fileName)
-  {
-    String returnValue = "";
-    FileReader file = null;
-    
     try {
-      file = new FileReader(fileName);
-      BufferedReader reader = new BufferedReader(file);
-      String line = "";
-      while ((line = reader.readLine()) != null) {
-	returnValue += line + "\n";
-      }
-    } catch (Exception e) {
-	throw new RuntimeException(e);
-    } finally {
-      if (file != null) {
-	try {
-	  file.close();
-	} catch (IOException e) {
-	  // Ignore issues during closing 
-	}
-      }
-    }
-    return returnValue;
-  } 
-
-  public void writeTextFile(String fileName, String s) {
-    FileWriter output = null;
-    try {
-      output = new FileWriter(fileName);
-      BufferedWriter writer = new BufferedWriter(output);
-      writer.write(s);
-    } catch (Exception e) {
+      loadStudents("students.txt"); 
+    } catch(FileNotFoundException e) {
+      System.out.println("Students.txt not found!");
+    } catch(IOException e) {
       throw new RuntimeException(e);
-    } finally {
-      if (output != null) {
-        try {
-          output.close();
-        } catch (IOException e) {
-          // Ignore issues during closing
-        }
-      }
     }
+  }
+
+  private void loadStudents(String fileName) throws IOException
+  {
+    BufferedReader file = new BufferedReader(new FileReader(fileName));
+    int numRecords = Integer.parseInt(file.readLine());
+
+    for(int i = 0; i < numRecords; i++)
+    {
+      String uid = file.readLine();
+      String sName = file.readLine();
+      String fName = file.readLine();
+      String degree = file.readLine();
+      Students.add(new Student(uid, sName, fName, degree));
+      System.out.println("Adding: " + uid + sName + fName + degree);
+    }
+
+    file.close();
   } 
 
   public void printReport()
